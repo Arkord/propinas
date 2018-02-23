4268d9a5cc1106d18a3efb7a7cbe6f39d0f41ff2
Octavio <arkord1@gmail.com>
2018-02-22 18:40:31 -0600

    config

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+    private costo: number;
+    private propina: number;
+    private totalPropina: number;
+    private totalGlobal: number
 
   constructor(public navCtrl: NavController) {
+    this.totalPropina = 0;
+    this.totalGlobal = 0;
+  }
 
+  CalcularPropina() {
+    this.totalPropina = this.costo * this.propina / 100;
+    this.totalGlobal = this.costo * 1 + this.propina * 1;
   }
 
 }
